807a0f3b8085e599684ad4cdc008b9d0551b0a84
Anoop T <anoop@live.co.uk>
2020-01-13 18:54:58 +0000

    updated imports

--- dist/index.js
+++ dist/index.js
@@ -57,50 +57,43 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const core = __importStar(__webpack_require__(682));
-const exec = __importStar(__webpack_require__(736));
-const io = __importStar(__webpack_require__(120));
+const core_1 = __webpack_require__(682);
+const exec_1 = __webpack_require__(736);
+const io_1 = __webpack_require__(120);
 let o365CLIPath;
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            core.info("Installing Office 365 CLI.");
+            core_1.info("Installing Office 365 CLI.");
             let o365CLIInstallCommand = "npm install -g @pnp/office365-cli";
             const options = {};
             options.silent = true;
             if (process.env.RUNNER_OS == "Windows") {
-                yield exec.exec(o365CLIInstallCommand, [], options);
+                yield exec_1.exec(o365CLIInstallCommand, [], options);
             }
             else {
-                yield exec.exec(`sudo ${o365CLIInstallCommand}`, [], options);
+                yield exec_1.exec(`sudo ${o365CLIInstallCommand}`, [], options);
             }
-            o365CLIPath = yield io.which("o365", true);
-            core.info("Completed installing Office 365 CLI.");
-            core.info("Logging in to the tenant.");
-            const username = core.getInput("USERNAME");
-            const password = core.getInput("PASSWORD");
+            o365CLIPath = yield io_1.which("o365", true);
+            core_1.info("Completed installing Office 365 CLI.");
+            core_1.info("Logging in to the tenant...");
+            const username = core_1.getInput("USERNAME");
+            const password = core_1.getInput("PASSWORD");
             yield executeO365CLICommand(`login --authType password --userName ${username} --password ${password}`);
             yield executeO365CLICommand("status");
-            core.info("Login successful.");
+            core_1.info("Login successful.");
         }
         catch (error) {
-            core.error("Login to the tenant failed. Please check the credentials. For more information refer https://aka.ms/create-secrets-for-GitHub-workflows");
-            core.setFailed(error);
+            error("Login to the tenant failed. Please check the credentials. For more information refer https://aka.ms/create-secrets-for-GitHub-workflows");
+            core_1.setFailed(error);
         }
     });
 }
 function executeO365CLICommand(command) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            yield exec.exec(`"${o365CLIPath}" ${command}`, [], {});
+            yield exec_1.exec(`"${o365CLIPath}" ${command}`, [], {});
         }
         catch (error) {
             throw new Error(error);
--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,43 @@
-import * as core from '@actions/core';
-import * as exec from '@actions/exec';
-import * as io from '@actions/io';
+import {getInput, info, error, setFailed, setOutput} from '@actions/core';
+import { exec } from '@actions/exec';
+import { which } from '@actions/io';
 
 let o365CLIPath: string;
 
 async function main() {
     try{
-        core.info("Installing Office 365 CLI.");
+        info("Installing Office 365 CLI.");
 
         let o365CLIInstallCommand: string = "npm install -g @pnp/office365-cli";
         const options: any = {};
         options.silent = true;
         if(process.env.RUNNER_OS == "Windows") {
-            await exec.exec(o365CLIInstallCommand, [], options);
+            await exec(o365CLIInstallCommand, [], options);
         } else {
-            await exec.exec(`sudo ${o365CLIInstallCommand}`, [], options);
+            await exec(`sudo ${o365CLIInstallCommand}`, [], options);
         }
-        o365CLIPath = await io.which("o365", true);
+        o365CLIPath = await which("o365", true);
         
-        core.info("Completed installing Office 365 CLI.");
+        info("Completed installing Office 365 CLI.");
 
-        core.info("Logging in to the tenant.");
+        info("Logging in to the tenant...");
 
-        const username: string = core.getInput("USERNAME");
-        const password: string = core.getInput("PASSWORD");
+        const username: string = getInput("USERNAME");
+        const password: string = getInput("PASSWORD");
         await executeO365CLICommand(`login --authType password --userName ${username} --password ${password}`);
         await executeO365CLICommand("status");
 
-        core.info("Login successful.");
+        info("Login successful.");
         
     } catch (error) {
-        core.error("Login to the tenant failed. Please check the credentials. For more information refer https://aka.ms/create-secrets-for-GitHub-workflows");
-        core.setFailed(error);
+        error("Login to the tenant failed. Please check the credentials. For more information refer https://aka.ms/create-secrets-for-GitHub-workflows");
+        setFailed(error);
     } 
 }
 
 async function executeO365CLICommand(command: string) {
     try {
-        await exec.exec(`"${o365CLIPath}" ${command}`, [],  {}); 
+        await exec(`"${o365CLIPath}" ${command}`, [],  {}); 
     }
     catch(error) {
         throw new Error(error);
